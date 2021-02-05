938760e85f7cf30967266eed53016812e3cd8391
jesuslerma <hyde@chuylerma.com>
2021-02-04 20:25:24 -0600

    Add final_market_price column to vehicle

---
+++ db/migrate/20210205022340_add_final_market_price_to_vehicle.rb
@@ -0,0 +1,8 @@
+class AddFinalMarketPriceToVehicle < ActiveRecord::Migration[6.1]
+  def change
+    add_column :vehicles, :final_market_price, :integer
+  end
+  def down
+    remove_column :vehicles, :final_market_price
+  end
+end
