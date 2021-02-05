1705c5a120a387cec156cc85ee5d501d099eabf9
jesuslerma <hyde@chuylerma.com>
2021-02-04 22:38:28 -0600

    Add case sensitive to uniqueness of vehicle model name

--- app/models/vehicle_model.rb
+++ app/models/vehicle_model.rb
@@ -4,7 +4,8 @@ class VehicleModel < ApplicationRecord
   belongs_to :brand, class_name: 'VehicleBrand'
   has_many :vehicles, dependent: :destroy, foreign_key: 'model_id', inverse_of: :model
   validates :name, presence: true
-  validates :name, uniqueness: true
   validates :brand_id, presence: true
   validates :market_price, numericality: { greater_than: 0 }
+
+  validates_uniqueness_of :name, case_sensitive: false
 end
