e78362a1242322f0f0d9f8dc588326053391319e
jesuslerma <hyde@chuylerma.com>
2021-02-04 22:30:54 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,83 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# Vehicle Brands
+["VW", "Chevrolet", "Nissan"].each do |name|
+  VehicleBrand.create(name: name)
+end
+
+# VehicleModels
+brand = VehicleBrand.find_by(name: "VW")
+[
+  {
+    brand: brand,
+    name: "JETTA",
+    market_price: 300000
+  },
+  {
+    brand: brand,
+    name: "POLLO",
+    market_price: 250000
+  },
+  {
+    brand: brand,
+    name: "GOLF",
+    market_price: 270000
+  },
+  {
+    brand: brand,
+    name: "UP",
+    market_price: 200000
+  },
+].each do |vehicle_model|
+  VehicleModel.create(vehicle_model)
+end
+
+brand = VehicleBrand.find_by(name: "Chevrolet")
+[
+  {
+    brand: brand,
+    name: "TRAILBLAZER",
+    market_price: 350000
+  },
+  {
+    brand: brand,
+    name: "SONIC",
+    market_price: 250000
+  },
+  {
+    brand: brand,
+    name: "BOLT",
+    market_price: 270000
+  },
+  {
+    brand: brand,
+    name: "BLAZER",
+    market_price: 300000
+  },
+].each do |vehicle_model|
+  VehicleModel.create(vehicle_model)
+end
+brand = VehicleBrand.find_by(name: "Nissan")
+[
+  {
+    brand: brand,
+    name: "KICKS",
+    market_price: 300000
+  },
+  {
+    brand: brand,
+    name: "SENTRA",
+    market_price: 250000
+  },
+  {
+    brand: brand,
+    name: "VERSA",
+    market_price: 270000
+  },
+  {
+    brand: brand,
+    name: "MURANO",
+    market_price: 200000
+  },
+].each do |vehicle_model|
+  VehicleModel.create(vehicle_model)
+end
